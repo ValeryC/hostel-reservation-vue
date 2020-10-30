8dfa050b9a003064d96396627c38146720ef1228
Chin Valery <chinvalery@gmail.com>
2020-10-30 07:56:29 +0100

    ádd firebase config

---
+++ src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <b-navbar fixed="top" toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand href="#">
+
+      </b-navbar-brand>
+    </b-navbar>
+    <b-navbar-toggle id="nav-collapse">  
+
+    </b-navbar-toggle>
+    <b-collapse id="nav-collapse" is-nav> 
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-button>
+            </b-button>
+          </b-nav-form>   
+        </b-navbar-nav>
+    </b-collapse>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
+
---
+++ src/firebase.js
@@ -0,0 +1,5 @@
+import firebase from "firebase/app"
+require("firebase/auth")
+require("firebase/firestore")
+require("firebase/storage")
+require("firebase/functions")
